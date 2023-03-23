a3c2f74e068e4930f17bf82bb2feb16ea4fdf070
Juan Carlos Rivas <carlos223651555@gmail.com>
2023-03-23 12:40:55 -0700

    Avance js

---
+++ js/main.js
@@ -0,0 +1,39 @@
+let mostrar = '';
+
+function pruebaDetalle() {
+
+    mostrar = `
+    <ul>
+            <li>Hola enzin</li>
+            <li>Aspirado de alfombra, asientos, cajuelas y limpieza de tablero.</li>
+            <li>Encerado.</li>
+            <li>Lavado de motor.</li>
+    </ul>
+    `
+
+    let detalle = document.getElementById('prueba');
+    detalle.innerHTML = mostrar;
+}
+
+function pruebaDetalle2() {
+
+    mostrar = `
+    <ul>
+            <li>Prueba2.</li>
+            <li>Es prueba.</li>
+            <li>Encerado.</li>
+            <li>Lavado de motor.</li>
+    </ul>
+    `
+
+    let detalle = document.getElementById('prueba');
+    detalle.innerHTML = mostrar;
+}
+
+
+
+window.addEventListener('DOMContentLoaded', pruebaDetalle)
+window.addEventListener('DOMContentLoaded', pruebaDetalle2)
+
+
+
